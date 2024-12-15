68528fdb863b7b26d92576e3641accb8474439ae
longtn-imt <longtn-imt@users.noreply.github.com>
2024-12-15 02:45:38 +0000

    deploy: 0930813adaa05b2c093aa50ba9e700ee287d7d4f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,11 +38,11 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/canvaskit.wasm": "e7602c687313cfac5f495c5eac2fb324",
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
-"index.html": "4771e7ac7a23e7829e7314ab2ad8a0b1",
-"/": "4771e7ac7a23e7829e7314ab2ad8a0b1",
+"index.html": "6561258932754bdd46a558968f5d72a4",
+"/": "6561258932754bdd46a558968f5d72a4",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "a57947c1229312914e367c925406048e",
+"flutter_bootstrap.js": "3b537a421193e99c895847d90d5a60ca",
 "main.dart.js": "4ddae3d4409a9ab2275aa3b12cd12723"};
 // The application shell files that are downloaded before a service worker can
 // start.
