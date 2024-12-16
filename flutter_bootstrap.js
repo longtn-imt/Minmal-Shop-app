beef8dd373a2f5eb434d3dca2525e1cd521fc46f
longtn-imt <longtn-imt@users.noreply.github.com>
2024-12-16 13:02:50 +0000

    deploy: e0c3aa21b23d0317972610e210c2f629735efd0e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "533080569"
+    serviceWorkerVersion: "3811603142"
   }
 });
